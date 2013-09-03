c060b6d4ac3365a93478a79a65c2ffeb21fda2d5
Vadim Zaliva <lord@crocodile.org>
2013-09-02 17:20:32 -0700

    registering NMEA listener

--- AltitudeRecorder/src/org/crocodile/altituderecorder/AltitudeRecordService.java
+++ AltitudeRecorder/src/org/crocodile/altituderecorder/AltitudeRecordService.java
@@ -5,12 +5,15 @@ import java.io.File;
 import java.io.IOException;
 
 import android.app.*;
+import android.content.Context;
 import android.content.Intent;
+import android.location.GpsStatus.NmeaListener;
+import android.location.LocationManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
-public class AltitudeRecordService extends Service
+public class AltitudeRecordService extends Service implements NmeaListener
 {
     private static final String DATA_FILE_SUFFIX   = ".txt";
     private static final String DATA_FILE_PREFIX   = "alt-";
@@ -70,8 +73,10 @@ public class AltitudeRecordService extends Service
                 fname = getNewLogFile();
                 Log.d(Constants.LOGTAG, "Will write data to file " + fname);
 
-                // TODO: start
+                LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                lm.addNmeaListener(this);
                 recording = true;
+
                 showNotification(START_NOTIFICATION);
                 sendBroadcast(Constants.SERVICE_STARTED_TOKEN, fname);
             } catch(IOException e)
@@ -86,7 +91,9 @@ public class AltitudeRecordService extends Service
     {
         if(recording)
         {
-            // TODO: stop
+            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            lm.removeNmeaListener(this);
+
             recording = false;
             showNotification(STOP_NOTIFICATION);
             sendBroadcast(Constants.SERVICE_STOPPED_TOKEN, fname);
@@ -121,6 +128,13 @@ public class AltitudeRecordService extends Service
         File fdir = this.getFilesDir();
         File f = File.createTempFile(DATA_FILE_PREFIX, DATA_FILE_SUFFIX, fdir);
         return f.getAbsolutePath();
+    }
+
+    @Override
+    public void onNmeaReceived(long timestamp, String nmea)
+    {
+        // TODO Auto-generated method stub
+
     }
 
 }
